f30a3095a72e1e8d55738e0c1eb56d0303916ad2
ammmar <ammaraslam7164@gmail.com>
2024-01-24 10:59:39 +0600

    footer setted

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Books from "./components/Books/Books";
 import Courses from "./components/Courses/Courses";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
       <Header></Header>
       <Books></Books>
       <Courses></Courses>
+        <Footer></Footer>
+      
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,15 @@ const withMT = require("@material-tailwind/react/utils/withMT");
 module.exports = withMT({
   content: [
     "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./src/**/*.{vite,js,ts,jsx,tsx}",
     "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
     "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extends:[
+
+    ]
+  }, 
+ 
+  plugins: [require("daisyui")],
 });
